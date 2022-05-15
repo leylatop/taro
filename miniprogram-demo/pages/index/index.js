e46237074653e51ae8529980d3bfa210eaa4da2d
qiaoxx <775177772@qq.com>
2022-05-15 16:01:40 +0800

    FEAT:  add miniprogram

---
+++ miniprogram-demo/pages/index/index.js
@@ -0,0 +1,45 @@
+var { Component, createPage } = require("../../npm/app.js")
+
+class Index extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    }
+  }
+  componentDidMount() {
+    console.log('执行componentDidMount');
+    this.setState({
+      count: 1
+    })
+  }
+  onAddClick() {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  onReduceClick() {
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
+
+  createData() {
+    this.__state = arguments[0];
+    const text = this.state.count % 2 === 0 ? '偶数' : '奇数';
+    Object.assign(this.__state, {
+      text: text
+    });
+    return this.__state;
+  }
+
+  render () {
+    const text = this.state.count % 2 === 0 ? '偶数' : '奇数'
+    return text
+  }
+}
+// 加入静态方法
+Index.$$events = ['onAddClick','onReduceClick']
+
+export default Page(createPage(Index))
